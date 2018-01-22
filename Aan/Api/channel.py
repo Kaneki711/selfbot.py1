f9bfe48bb8c2b0b9d24a6df7b61e00bc98016535
AanJutawan <33293274+AanJutawan@users.noreply.github.com>
2018-01-22 11:55:06 +0000

    Update channel.py

--- Aan/Api/channel.py
+++ Aan/Api/channel.py
@@ -18,8 +18,8 @@ class Channel:
     http_query_path = "/S4"
     channel_query_path = "/CH4"
 
-    UA = "Line/7.18.0"
-    LA = "CHROMEOS\t7.18.0\tChrome_OS\t1"
+    UA = "Line/8.0.1 iPad4,1 9.0.2"
+    LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
     
    
     authToken = None
