e21763854a9ebd4faf80865cd7d0a87671ac00b1
AanJutawan <33293274+AanJutawan@users.noreply.github.com>
2018-01-22 11:48:21 +0000

    Update LineApi.py

--- Aan/LineApi.py
+++ Aan/LineApi.py
@@ -42,9 +42,9 @@ class LINE:
     self.authToken = self.Talk.authToken
     self.cert = self.Talk.cert
     self._headers = {
-              'X-Line-Application': 'CHROMEOS\t7.18.0\tChrome_OS\t1',
+              'X-Line-Application': 'DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0',
               'X-Line-Access': self.authToken, 
-              'User-Agent': 'Line/7.18.0'
+              'User-Agent': 'Line/8.0.1 iPad4,1 9.0.2'
    }
     self.Poll = Poll(self.authToken)
     self.channel = channel.Channel(self.authToken)
