589f9b58fddab4de84aabd01aaf8bc5fbac5b4c1
AanJutawan <33293274+AanJutawan@users.noreply.github.com>
2018-01-22 11:52:22 +0000

    Update Talk.py

--- Aan/Api/Talk.py
+++ Aan/Api/Talk.py
@@ -20,8 +20,8 @@ class Talk:
   host = "gd2.line.naver.jp";
   port = 443;
       
-  UA = "Line/7.18.0"
-  LA = "CHROMEOS\t7.18.0\tChrome_OS\t1"
+  UA = "Line/8.0.1 iPad4,1 9.0.2"
+  LA = "DESKTOPMAC 10.10.2-YOSEMITE-x64    MAC 4.5.0"
 
 
   authToken = None
